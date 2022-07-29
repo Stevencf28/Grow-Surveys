26744d194d2b4006fa96437f98c66c674cb27bff
Steven Chen <schen196@my.centennialcollege.ca>
2022-07-29 18:09:25 -0400

    Fixed back-end not communicating with Front-end and added swagger

--- apiproject/Program.cs
+++ apiproject/Program.cs
@@ -1,14 +1,19 @@
 using apiproject.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<SurveyContext>(options =>
+builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("Connection2RDS"));
+});
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "GrowSurveysAPI", Version = "v1" });
 });
 
 var app = builder.Build();
@@ -18,6 +23,8 @@ if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GrowSurveysAPI v1"));
 }
 
 app.UseHttpsRedirection();

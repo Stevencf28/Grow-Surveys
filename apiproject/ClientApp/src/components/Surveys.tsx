9edafd40a740271dfec5f04b5f91d25d368229ca
Steven Chen <schen196@my.centennialcollege.ca>
2022-08-02 19:31:02 -0400

    Working API for GET, GETBYID, PUT, POST, DELETE

--- apiproject/ClientApp/src/components/Surveys.tsx
+++ apiproject/ClientApp/src/components/Surveys.tsx
@@ -1,69 +1,104 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Survey from "../interface/surveyInterface";
+import User from "../interface/userInterface";
 
-export interface Survey {
-	id: number;
-	title: string;
-}
 
 export default function Surveys() {
-	const surveyList: Survey[] = [
-		{
-			id: 1,
-			title: "Title 1",
-		},
-		{
-			id: 2,
-			title: "Title 2",
-		},
-		{
-			id: 3,
-			title: "Title 3",
-		},
-		{
-			id: 4,
-			title: "Title 4",
-		},
-		{
-			id:5,
-			title: "Title 5",
-		},
-		{
-			id: 6,
-			title: "Title 6",
-		},
-	];
+	const [surveys, setSurveys] = useState<Survey[]>([]);
+	const [user, setUser] = useState<User | null>();
+	const navigate = useNavigate();
 
-	const [surveys, setSurveys] = useState(surveyList);
+	useEffect( () => {
+		const url = 'https://localhost:7214/Survey';
+	
+		const fetchData = async () => {
+			try{
+				const response = await fetch(url);
+				const data = await response.json();
+				setSurveys(data);
+			}
+			catch (error){
+				console.log("error", error);
+			}
+		}
+		fetchData();
 
-	return (
-		<div className='flex flex-col items-center gap-40 mt-40'>
-			<h1 className="mt-6 text-center text-3xl font-bold text-gray-900">Available Surveys</h1>
+		const retrieveUser = localStorage.getItem("user");
+		if (retrieveUser){
+			setUser(JSON.parse(retrieveUser));
+		} 
+		else{
+			setUser(null);
+		}
+	}, []);
 
-			<table className="table-auto w-1/2 mx-auto border-separate border-2 border-spacing-0 rounded-md border-gray-800">
-				<thead>
-					<tr className='bg-gray-800 text-gray-300'>
-						<th className='rounded-tl-sm p-2'>Id</th>
-						<th className='p-2'>Title</th>
-						<th className='rounded-tr-sm p-2'>Action</th>
-					</tr>
-				</thead>
-				<tbody>
-					{
-						surveys.map(({ id, title }) => (
-							<tr>
-								<td className='border-t-2 border-gray-500 text-center px-4 py-2 border-r-2'>{id}</td>
-								<td className='border-t-2 border-gray-500 text-center px-4 py-2 border-r-2'>{title}</td>
-								<td className='border-t-2 border-gray-500 text-center px-4 py-2'>
-									<button className="border border-transparent text-orange-600 font-bold hover:text-orange-500 rounded-md">
-										<Link to={id.toString()}>Answer</Link>
-									</button>
-								</td>
+	const deleteSurvey = async (surveyId: number) => {
+		const url = 'https://localhost:7214/Survey/delete/' + surveyId;
+		console.log(url)
+		const requestOptions = {
+			method: 'DELETE'
+		}
+		fetch (url, requestOptions)
+			.then(response => response.json())
+			.then(data => console.log(data))
+			.catch(err => console.log(err));
+		navigate(0);
+	}
+
+	if (user != null){
+		return (
+			<>
+				<div className='flex flex-col items-center gap-40 mt-40'>
+					<h1 className="mt-6 text-center text-3xl font-bold text-gray-900">Available Surveys</h1>
+	
+					<table className="table-auto w-1/2 mx-auto border-separate border-2 border-spacing-0 rounded-md border-gray-800">
+						<thead>
+							<tr className='bg-gray-800 text-gray-300'>
+								<th className='rounded-tl-sm p-2'>Id</th>
+								<th className='p-2'>Title</th>
+								<th className='rounded-tr-sm p-2'>Action</th>
 							</tr>
-						))
-					}
-				</tbody>
-			</table>
-		</div>
-	);
+						</thead>
+						<tbody>
+							{
+								surveys &&
+								surveys.map(survey => 
+									{
+										return (
+											<tr key={survey.surveyId}>
+												<td className='border-t-2 border-gray-500 text-center px-4 py-2 border-r-2'>{survey.surveyId}</td>
+												<td className='border-t-2 border-gray-500 text-center px-4 py-2 border-r-2'>{survey.title}</td>
+												<td className='border-t-2 border-gray-500 text-center px-4 py-2'>
+													<button className="border border-transparent text-orange-600 font-bold hover:text-orange-500 rounded-md">
+														<Link to={survey.surveyId.toString()}>Answer</Link>
+													</button>
+													{
+														user?.userId.toString() === survey.userId.toString() &&
+														<div>
+															<button 
+															className="border border-transparent text-red-600 font-bold hover:text-red-500 rounded-md"
+															onClick={() => deleteSurvey(survey.surveyId)}>
+																DELETE
+															</button>
+														</div>
+															
+													}
+												</td>
+											</tr>
+										)
+									}) 
+							}
+						</tbody>
+					</table>
+				</div>
+			</>
+		);
+	} 
+	else
+	{
+		navigate("/login", {replace: true});
+		navigate(0);
+		return(<></>);
+	}
 }
---
+++ apiproject/Controllers/SurveyController.cs
@@ -0,0 +1,101 @@
+﻿using apiproject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace apiproject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SurveyController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public SurveyController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Get Surveys
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Survey>>> GetSurveys()
+        {
+            return await _context.Surveys.ToListAsync();
+        }
+
+
+        // Get Survey by ID
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Survey>> GetSurvey(int id)
+        {
+            var survey = await _context.Surveys.FindAsync(id);
+
+            if (survey == null)
+            {
+                return NotFound();
+            }
+
+            return survey;
+        }
+
+        // Update Survey
+        [HttpPut("/update/{id:int}")]
+        public async Task<IActionResult> PutSurvey(int id, Survey survey)
+        {
+            if (id != survey.SurveyId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(survey).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SurveyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // Create Survey
+        [HttpPost("/create")]
+        public async Task<ActionResult<Survey>> CreateSurvey(Survey survey)
+        {
+            _context.Surveys.Add(survey);
+            await _context.SaveChangesAsync();
+
+            return survey;
+        }
+
+        [HttpDelete("/delete/{id:int}")]
+        public async Task<ActionResult> DeleteSurvey(int id)
+        {
+            var survey = await _context.Surveys.FindAsync(id);
+
+            if (survey == null)
+            {
+                return NotFound();
+            }
+            _context.Attach(survey);
+            _context.Remove(survey);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        // Check if Survey exists
+        private bool SurveyExists(int surveyId)
+        {
+            return (_context.Surveys?.Any(e => e.SurveyId == surveyId)).GetValueOrDefault();
+        }
+    }
+}
